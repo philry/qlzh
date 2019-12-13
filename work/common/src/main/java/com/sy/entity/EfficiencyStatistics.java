08a16dc27cd8d19817563bd26ccf39a69c4ec3d2
chenkai <313411703@hotmail.com>
2019-12-13 08:43:51 +0800

    新增类

---
+++ work/common/src/main/java/com/sy/entity/EfficiencyStatistics.java
@@ -0,0 +1,185 @@
+package com.sy.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "efficiency_statistics", schema = "qlzh", catalog = "")
+public class EfficiencyStatistics {
+    private int id;
+    private String name;
+    private Integer level;
+    private Integer pid;
+    private Integer time;
+    private Integer workingTime;
+    private String efficiency;
+    private Double power;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date date;
+    private String status;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Timestamp createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Timestamp updateTime;
+    private String remark;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "level")
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    @Basic
+    @Column(name = "pid")
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    @Basic
+    @Column(name = "time")
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    @Basic
+    @Column(name = "working_time")
+    public Integer getWorkingTime() {
+        return workingTime;
+    }
+
+    public void setWorkingTime(Integer workingTime) {
+        this.workingTime = workingTime;
+    }
+
+    @Basic
+    @Column(name = "efficiency")
+    public String getEfficiency() {
+        return efficiency;
+    }
+
+    public void setEfficiency(String efficiency) {
+        this.efficiency = efficiency;
+    }
+
+    @Basic
+    @Column(name = "power")
+    public Double getPower() {
+        return power;
+    }
+
+    public void setPower(Double power) {
+        this.power = power;
+    }
+
+    @Basic
+    @Column(name = "date")
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Basic
+    @Column(name = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "create_time")
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "update_time")
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Basic
+    @Column(name = "remark")
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EfficiencyStatistics that = (EfficiencyStatistics) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(level, that.level) &&
+                Objects.equals(pid, that.pid) &&
+                Objects.equals(time, that.time) &&
+                Objects.equals(workingTime, that.workingTime) &&
+                Objects.equals(efficiency, that.efficiency) &&
+                Objects.equals(power, that.power) &&
+                Objects.equals(date, that.date) &&
+                Objects.equals(status, that.status) &&
+                Objects.equals(createTime, that.createTime) &&
+                Objects.equals(updateTime, that.updateTime) &&
+                Objects.equals(remark, that.remark);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, level, pid, time, workingTime, efficiency, power, date, status, createTime, updateTime, remark);
+    }
+}
