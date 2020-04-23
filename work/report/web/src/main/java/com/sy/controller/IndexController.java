8b32d3b1cde87821a761c43f38216393b1fbb282
chenkai <313411703@hotmail.com>
2020-04-23 08:08:45 +0800

    indexController新增"/appdata"接口（套用"/data"）

--- work/report/web/src/main/java/com/sy/controller/IndexController.java
+++ work/report/web/src/main/java/com/sy/controller/IndexController.java
@@ -230,6 +230,193 @@ public class IndexController {
         return result;
     }
 
+    @RequestMapping(value = "appdata",method = RequestMethod.GET)
+    public AjaxResult getAppData(){
+
+        Date now = new Date();
+        String today = DateUtils.parseDateToStr(DateUtils.YYYY_MM_DD, now);
+        String day = DateUtils.getPrevDay(today);
+
+        //在岗人数(查询work表,查询当日扫码的人员的id的个数)
+        List<Integer> work_day = workDao.getPersonIdsByDate(today);
+        int work_day_counts = work_day.size();
+        //环比(查询work表,查询昨日扫码的人员的id的个数)
+        int pre_work_day_counts = workDao.getPersonIdsByDate(day).size();
+        //总人数(根据人员表获取总人数)
+        int person_counts = personDao.findAll().size();
+        String workerProportion = null;
+        if(person_counts==0){
+            workerProportion = (String.format("%.2f", (double)(work_day_counts-pre_work_day_counts)*100));
+        }else {
+            workerProportion = (String.format("%.2f", (double)(work_day_counts-pre_work_day_counts)/pre_work_day_counts*100));
+        }
+        //今日工人工效(调用工效查询接口，直接获取最上级数据)
+        double todayEfficiency = Double.parseDouble(getEfficiency(today));
+        double dayEfficiency = Double.parseDouble(getEfficiency(day));
+        String efficiencyProportion = null;
+        if(person_counts==0){
+            efficiencyProportion = (String.format("%.2f", (todayEfficiency-dayEfficiency)*100));
+        }else {
+            efficiencyProportion = (String.format("%.2f", (todayEfficiency-dayEfficiency)/dayEfficiency*100));
+        }
+        //焊机总数
+        int machineCounts = machineDao.findAll().size();
+        //实时焊机数(查询machineNow表,获取个数)
+        List<MachineNow> machineNowList = machineNowDao.findAll();
+        int machineNowCounts = machineNowDao.findAll().size();
+        //实时焊机工作数(根据实时打开的焊机的xpg,获取最新的netty同步数据,根据电流判定是否在工具)
+        int machineUseCounts = workDao.getMachineId(today).size();
+
+        //用电量(调用工程查询接口)
+        List<EfficiencyStatisticsVo> efficiencyStatisticsVos = new ArrayList<>();
+        try {
+            efficiencyStatisticsVos = statisticsService.getAllData("",DateUtils.parseDate(today),DateUtils.parseDate(today));
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        //今日工程耗能(调用工程查询接口)
+        Set<IndexVo> indexVosProject = new HashSet<>();
+        double todayPower = 0.0;
+        if(efficiencyStatisticsVos!=null&&!efficiencyStatisticsVos.isEmpty()){
+            for (EfficiencyStatisticsVo vo : efficiencyStatisticsVos) {
+                todayPower += vo.getPower();
+                IndexVo indexVo = new IndexVo();
+                indexVo.setName(vo.getName());
+                indexVo.setPower(String.valueOf(vo.getPower()));
+                indexVo.setWorkNo(taskDao.getWorkNoByName(vo.getName()));
+                indexVosProject.add(indexVo);
+            }
+        }
+        //本月总用电量(调用工程查询接口)
+        String monthFirstDay = DateUtils.getMonthFirstDay();
+        try {
+            efficiencyStatisticsVos = statisticsService.getAllData("",DateUtils.parseDate(monthFirstDay),DateUtils.parseDate(today));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        double totalPower = 0.0;
+        System.out.println(efficiencyStatisticsVos);
+        if(efficiencyStatisticsVos!=null&&!efficiencyStatisticsVos.isEmpty()){
+            for (EfficiencyStatisticsVo vo : efficiencyStatisticsVos) {
+                totalPower += vo.getPower();
+            }
+        }
+        //两周图表
+        List<String> dayStrings = new ArrayList<>();
+        int length = 14;
+        String tempDay = today;
+        dayStrings.add(tempDay);
+        for (int i = 0; i <length-1 ; i++) {
+            tempDay = DateUtils.getPrevDay(tempDay);
+            dayStrings.add(tempDay);
+        }
+        List<ChartVo> chartResult = new ArrayList<>();
+        for (String dayString : dayStrings) {
+            ChartVo vo = new ChartVo();
+            vo.setDate(dayString);
+            try {
+                List<EfficiencyStatisticsVo> vos = statisticsService.getAllData("",DateUtils.parseDate(dayString),DateUtils.parseDate(dayString));
+                double power = 0.0;
+                if(!vos.isEmpty()){
+                    for (EfficiencyStatisticsVo efficiencyStatisticsVo : vos) {
+                        power += efficiencyStatisticsVo.getPower();
+                    }
+                }
+                vo.setPowerValue(String.valueOf(power));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            List<EngineeringVo> engineeringVos = engineeringService.getInitData(DateUtils.parseDate(dayString),DateUtils.parseDate(dayString));
+            if(!engineeringVos.isEmpty()){
+                EngineeringVo engineeringVo = engineeringVos.get(0);
+                vo.setRateValue(String.format("%.2f", (double)(engineeringVo.getWorkTime())/engineeringVo.getTime()*100));
+            }
+            chartResult.add(vo);
+        }
+        //工效表(调用工效查询接口)
+        List<EngineeringVo> vos = new ArrayList<>();
+        try {
+            vos = engineeringService.getInitData(DateUtils.parseDate(today),DateUtils.parseDate(today));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //班组工效(调用工效查询接口)
+        int time = 0;
+        int workTime = 0;
+        //今日工效
+        Set<IndexVo> indexVoEfficiency = new HashSet<>();
+        if(vos!=null&&!vos.isEmpty()){
+            for (EngineeringVo vo : vos) {
+                time += vo.getTime_2();
+                workTime += vo.getWorkTime_2();
+                IndexVo indexVo = new IndexVo();
+                indexVo.setDate(today);
+                indexVo.setName(vo.getName_2());
+                indexVo.setEfficiency(String.format("%.2f", (double)(vo.getWorkTime_2())/vo.getTime_2()*100));
+                indexVoEfficiency.add(indexVo);
+            }
+        }
+        //昨日工效
+        try {
+            vos = engineeringService.getInitData(DateUtils.parseDate(day),DateUtils.parseDate(day));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Set<IndexVo> indexVoPrevEfficiency = new HashSet<>();
+        if(vos!=null&&!vos.isEmpty()){
+            for (EngineeringVo vo : vos) {
+                IndexVo indexVo = new IndexVo();
+                indexVo.setDate(today);
+                indexVo.setName(vo.getName_2());
+                indexVo.setEfficiency(String.format("%.2f", (double)(vo.getWorkTime_2())/vo.getTime_2()*100));
+                indexVoPrevEfficiency.add(indexVo);
+            }
+        }
+
+        if(!indexVoEfficiency.isEmpty()&&!indexVoPrevEfficiency.isEmpty()){
+            for (IndexVo indexVo : indexVoEfficiency) {
+                for (IndexVo vo : indexVoPrevEfficiency) {
+                    if(vo.getName().equals(indexVo.getName())){
+                        indexVo.setPervEfficiency(vo.getEfficiency());
+                    }
+                }
+            }
+        }
+
+        //总工效
+        AppScan appScan = new AppScan();
+        appScan.setEfficiency(chartResult.get(0).getRateValue());
+        try {
+            double dayEff = Double.parseDouble(chartResult.get(0).getRateValue());
+            double prevDayEff = Double.parseDouble(chartResult.get(1).getRateValue());
+            appScan.setProportion(String.format("%.2f", (dayEff-prevDayEff)*100/prevDayEff));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        appScan.setTime(time);
+        appScan.setWork_time(workTime);
+
+        AjaxResult result = new AjaxResult();
+        result.setMsg("操作成功");
+        result.setCode(200);
+        result.put("todayWorkCount", work_day_counts);//今日在岗人数
+        result.put("yesterdayWorkCount", pre_work_day_counts);//昨日在岗人数
+        result.put("openCount", machineNowCounts);   //实时焊机数
+        result.put("workCount", machineUseCounts);   //实时焊机工作数
+        result.put("totalCount", machineCounts);      //焊机总数
+        result.put("todayUsedPower", todayPower);      //今日工程耗能
+        result.put("totalWorkCount", person_counts);    //总人数
+        result.put("currentMonthUsedPower", totalPower);  //本月总用电量
+        result.put("chartResult", chartResult);          //两周图表
+        result.put("projectResult", indexVosProject);     //今日工程耗能
+        result.put("groupResult", indexVoEfficiency);     //今日工效
+        result.put("appScan",appScan);                 //app总工效等
+        return result;
+    }
+
+
     private String getEfficiency(String today) {
         List<EngineeringVo> engineeringVos = engineeringService.getInitData(DateUtils.parseDate(today),DateUtils.parseDate(today));
         if (engineeringVos.isEmpty()){
