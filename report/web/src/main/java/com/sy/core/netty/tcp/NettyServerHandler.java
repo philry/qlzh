837ba6c7c137792818ac3300870d88486c86bfc0
philry <123>
2020-08-19 12:40:06 +0800

    nettyServerHandler的开关机hex16日志打印

--- report/web/src/main/java/com/sy/core/netty/tcp/NettyServerHandler.java
+++ report/web/src/main/java/com/sy/core/netty/tcp/NettyServerHandler.java
@@ -433,7 +433,7 @@ public class NettyServerHandler extends ChannelHandlerAdapter {
 
 		Channel ctx = ClientChannel.getChannel(xpg);
 		System.out.println("-----------ctx = "+ctx+"-----------");
-
+		System.out.println("-----------hex16 = "+hex16+"-----------");
 		if(ctx==null){
 			throw new Exception(xpg+"连接尚未建立,请稍后再试");
 		}
