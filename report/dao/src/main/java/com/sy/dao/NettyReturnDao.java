4ced74b93b3cdc8ace38679449a6b31e06cd2a40
101.133.171.77 <132>
2020-11-02 17:00:08 +0800

    格朗设备对接中

---
+++ report/dao/src/main/java/com/sy/dao/NettyReturnDao.java
@@ -0,0 +1,35 @@
+package com.sy.dao;
+
+import com.sy.entity.Netty;
+import com.sy.entity.NettyReturn;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Repository
+public interface NettyReturnDao extends JpaRepository<NettyReturn,Integer>, JpaSpecificationExecutor {
+
+    List<Netty> getAllByCreateTimeBetween(Date beginTime, Date endTime);
+    
+    @Query(value = "select * from netty_return  where xpg = ?1 ORDER BY create_time desc",nativeQuery = true)
+    List<Netty> getNettyByXpg(String xpg);
+
+    @Query(value = "select distinct xpg from netty_return where create_time between ?1 and ?2",nativeQuery = true)
+    List<String> findAllXpgs(Date beginTime, Date endTime);
+
+    void deleteById(Integer id);
+
+//    @Query(value = "delete from netty_return where date < ?1",nativeQuery = true)
+    @Query(value = "delete from netty_return where create_time < ?1",nativeQuery = true)
+    @Modifying()
+    void deleteByDate(Date time);
+
+    @Query(value = "select * from netty_return where xpg = ?1 and create_time >= ?2 ORDER BY create_time desc LIMIT 1",nativeQuery = true)
+    Netty getLastNettyByXpgAndOpenTime(String xpg, Date openTime);
+}
